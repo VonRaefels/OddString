1766e5cfacf3c638b7848e215b689c09b7238233
Chutlhu <diego.dicarlo89@gmail.com>
2016-03-18 13:53:29 +0100

    Initial Commit

--- OddStringSensorsData.ino
+++ OddStringSensorsData.ino
@@ -14,7 +14,6 @@ SLIPEncodedUSBSerial SLIPSerial( thisBoardsSerialUSB );
 
 
 /* Knock Sensor
-
 created 29 Feb 2016
 by Diego Di Carlo and Jorge Madrid Portillo
 modified 29 Feb 2016
@@ -22,49 +21,57 @@ by Diego Di Carlo
 */
 
 // these constants won't change:
-const int knockSensorA1 = A1;
-const int knockSensorA2 = A2;
-const int knockSensorA3 = A3;
+const int piezoPinA1 = A1;
+const int piezoPinA2 = A2;
+const int piezoPinA3 = A3;
 
-const int softPotSensor = A0;
+const int softPotPin = A0;
 
 // these variables will change:
-int sensorReadingA1 = 0;
-int sensorReadingA2 = 0;
-int sensorReadingA3 = 0;
-int sensorReadingA0 = 0;
-int thr = 500;
+int sensorValueA0 = 0;
+int sensorValueA1 = 0;
+int sensorValueA2 = 0;
+int sensorValueA3 = 0;
+
+int piezoThr = 500;
+int softPotThr = 813;
 
 void setup() {
-   //begin SLIPSerial just like Serial
-  SLIPSerial.begin(9600);   // set this as high as you can reliably run on your platform
-  #if ARDUINO >= 100
-    while(!Serial)
+
+    //enable pullup resistor
+    digitalWrite(softPotPin, HIGH); // to avoid open-circuit interference
+    
+    //begin SLIPSerial just like Serial
+    SLIPSerial.begin(9600);   // set this as high as you can reliably run on your platform
+    #if ARDUINO >= 100
+      while(!Serial)
       ; //Leonardo "feature"
-  #endif
+    #endif
 }
 
 
 void loop() {
+  
     // read the sensor and store it in the variable sensorReading:
-    sensorReadingA1 = analogRead(knockSensorA1); 
-    if (sensorReadingA1 >= thr){
-      Serial.write("/piezo/01 ");
-
+    sensorValueA0 = analogRead(softPotPin);
+    if (sensorValueA0 <= softPotThr){
+      Serial.println(sensorValueA0);
     }
-
-    sensorReadingA2 = analogRead(knockSensorA2);
-    if (sensorReadingA2 >= thr){
-      //SLIPSerial.println("SensorA2 " + String(sensorReadingA2));
-    }
-
-    sensorReadingA3 = analogRead(knockSensorA3);
-    if (sensorReadingA3 >= thr){
-      //SLIPSerial.println("SensorA3 " + String(sensorReadingA3));
-    }
-
-    sensorReadingA0 = analogRead(softPotSensor);
-    Serial.write(sensorReadingA0);
     
-    delay(100);
+    sensorValueA1 = analogRead(piezoPinA1); 
+    if (sensorValueA1 >= piezoThr){
+      Serial.write("/piezo/01 \n");
+    }
+
+    sensorValueA2 = analogRead(piezoPinA2);
+    if (sensorValueA2 >= piezoThr){
+      SLIPSerial.println("Piezo 2 " + String(sensorValueA2));
+    }
+
+    sensorValueA3 = analogRead(piezoPinA3);
+    if (sensorValueA3 >= piezoThr){
+      //SLIPSerial.println("Piezo 3 " + String(sensorValueA3));
+    }
+    
+    delay(50);
 }
